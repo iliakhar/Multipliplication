9b7863d12e3e14ff0559e6d5064bb2f8abcf4646
Илья Харченко <kharchenko_ilia@mail.ru>
2022-11-11 17:37:37 +0700

    Simple mult has been completed

--- Multipliplication/Multipliplication.py
+++ Multipliplication/Multipliplication.py
@@ -3,7 +3,7 @@ import math
 N1 = 0
 
 def SumListNum(a, b):
-    global N1
+    #global N1
     for i in range(0, len(a)-len(b), 1):
         b.insert(0,0)
     for i in range(0, len(b)-len(a), 1):
@@ -11,7 +11,7 @@ def SumListNum(a, b):
     answ = []
     numSum = digit = 0
     for i in range(len(a)-1, -1, -1):
-        N1 += 1
+        #N1 += 1
         numSum = a[i] + b[i] + digit
         digit = int(math.floor(numSum/10))
         answ.insert(0,numSum % 10)
@@ -75,23 +75,46 @@ def mult(a, b, isFast = False):
 def ColumnMult(a, b):
     answ = []
     global N1
-    N1 = 0
+    N1 = countWrToAnsw = 0
     for i in range(len(b)):
         remB = b[len(b)-1-i]
         digit = 0
         miniAnsw = [0]*i
-        for j in range(len(a)):
-            N1+=2
-            remA = a[len(a)-1-j]
-            multNum = remA * remB + digit
-            digit = math.floor(multNum / 10)
-            miniAnsw.insert(0,math.floor(multNum % 10))
-        if digit != 0: miniAnsw.insert(0, digit)
-        answ = SumListNum(answ, miniAnsw)
+        if remB != 0:
+            for j in range(len(a)):
+                
+                remA = a[len(a)-1-j]
+                if remA != 0 or digit != 0:
+                    N1+=2
+                    multNum = remA * remB + digit
+                    digit = math.floor(multNum / 10)
+                    miniAnsw.insert(0,math.floor(multNum % 10))
+                else: miniAnsw.insert(0,0)
+            if digit != 0: miniAnsw.insert(0, digit)
+            if countWrToAnsw != 0:
+                answ = SumListNum(answ, miniAnsw)
+                N1 += 1
+            else: answ = miniAnsw
+            countWrToAnsw += 1
     return answ
 
+def StrToList(s):
+    lst = []
+    for i in s:
+        lst.append(ord(i) - ord('0'))
+    return lst
+
+def GetNumbers():
+    print("Enter First Num: ", end = "");
+    a = input()
+    print("\nEnter Second Num: ", end = "");
+    b = input()
+    print()
+    a1 = StrToList(a)
+    return a1,StrToList(b)
 
 #print(mult([9,8], [9,8]))
 #print(mult(43, 2512, True))
-print(ColumnMult([4,3,3,3,9,8], [2,5,1,2,8,7]))
-print("N1:",N1)
+num1, num2 = GetNumbers()
+print(ColumnMult(num1, num2))
+print("N1: ",N1)
